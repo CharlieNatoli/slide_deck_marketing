359844cf24180ec0f343bb1a0134a7ddfdf62d05
Vinicius Aurichio <vinicius.aurichio@klaviyo.com>
2020-05-02 12:30:40 -0400

    Flexible content generation

--- parse_strings.py
+++ parse_strings.py
@@ -1,2 +1,19 @@
+import numpy as np
+
 def add_bluh(s):
     return ', bluh.'.join(s.split('.'))
+
+
+def add_bruh(s):
+    return ', bruh.'.join(s.split('.'))
+
+
+func_list = [
+        add_bluh,
+        add_bruh,
+        ]
+
+def random_parse_string(s):
+    func = np.random.choice(func_list)
+    return func(s)
+
