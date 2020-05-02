02f3e915f9ce619fe0d33b4f138cb33a788d3a71
Charlie Natoli <charlie.natoli1@gmail.com>
2020-05-02 15:04:51 -0400

    add text box

--- slidehub_dot_ai.py
+++ slidehub_dot_ai.py
@@ -1,4 +1,6 @@
 
+import numpy as np
+
 from authenticate import HorribleGoogleAPIService
 from slide_file_utilities import HorribleSlideDeckEditor
 from file_sharing import HorribleGoogleDriveHandler
@@ -11,10 +13,15 @@ def get_emails():
     email_list = email_list.split(',')
     return [x.strip() for x in email_list]
 
+def get_user_input():
+    email_list = get_emails()
+    message_text = input('Now, please enter what it is that your contacts must know: ')
+    return email_list, message_text
+
 
 if __name__ == '__main__':
 
-    email_list = get_emails()
+    email_list, message_text = get_user_input()
 
     api_service = HorribleGoogleAPIService()
 
@@ -22,6 +29,9 @@ if __name__ == '__main__':
     google_drive_handler = HorribleGoogleDriveHandler(api_service)
 
     deck_info = slide_deck_editor.create_slide_deck()
+
+    for i in range(np.random.randint(1, 10)):
+        slide_deck_editor.create_text_box(deck_info, message_text)
 
     for email in email_list:
         google_drive_handler.share_file(deck_info['presentationId'], email)
